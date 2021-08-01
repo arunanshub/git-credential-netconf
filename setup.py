a054bafa0ae6aa160f43c0c90bcddad30084f265
Arunanshu Biswas <mydellpc07@gmail.com>
2021-08-01 20:23:05 +0530

    bump: version 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="git-credential-netconf",
-    version="1.0.2",
+    version="1.0.3",
     author="Arunanshu Biswas",
     author_email="mydellpc07@gmail.com",
     description="",
